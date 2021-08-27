dc36c146266654e6f48588e3bede3394e535d627
Rodrigo <A01651416@itesm.mx>
2021-08-27 11:13:57 -0500

    for has indexes and we have started giving values to dictionary

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+#\
 # simple transition function method
 
 # Union Method
@@ -11,7 +12,6 @@ def main():
     dS = []
     for i in desiredString: #splits desiredString into chars
         dS.append(i)
-    print(dS)
     #dictionary without values
     transitionTable = {
         "states": None, 
@@ -21,9 +21,23 @@ def main():
         "evaluation": None
     } 
     file = open("test1.txt", "r")
-    for x in file:
-        print(x)
-
+    # \n needs to be splitted
+    # evaluation key needs to be filled maybe another for
+    for idx, x in enumerate(file):
+        splitString = x.split(",")
+        if idx == 0:
+            transitionTable["states"] = splStr
+        elif idx == 1: 
+            transitionTable["alphabet"] = splitString
+        elif idx == 2: 
+            transitionTable["initialState"] = splitString
+        elif idx == 3: 
+            transitionTable["finalStates"] = splitString                        
+        else: 
+            pass
+            
+    
+    print(transitionTable)
     file.close() 
 
 
