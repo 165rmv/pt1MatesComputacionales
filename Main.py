4dd442d6ea1d9ee13de0cfc0372de5449507d023
165rmv <47989205+165rmv@users.noreply.github.com>
2021-09-19 20:04:48 -0500

    user string is validated

--- Main.py
+++ Main.py
@@ -27,12 +27,22 @@ def fillDictionary(file):
 		else:
 			pass
 
-# String validator
+# user String validator
 def inDictionary():
-	"""
-	leng_dict= len(transitionTable['alphabet'])
-	print(leng_dict)
-	"""
+	s1 = set(transitionTable["alphabet"])
+	s2 = set(dS)
+	if len(s2) == 1:
+		print(s2, "only one char")
+		if(s2 & s1):
+			print("GOOD String")
+		else: 
+			print("BAD string")
+	else:
+		print("more than one char")
+		if(s1 == s2):
+			print(s2, "sets are the same GOOD")
+		else:
+			print(s2, "BAD string")
 
 # simple transition function method
 def simTranFunc():
@@ -53,8 +63,9 @@ def main():
 		dS.append(i)
 	file = open("test1.txt", "r")
 	fillDictionary(file)
-	pprint.pprint(transitionTable)
 	file.close()
+	pprint.pprint(transitionTable)
+	inDictionary()
 
 if __name__ == "__main__": 
     main()
